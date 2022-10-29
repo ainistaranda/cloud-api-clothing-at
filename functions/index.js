fdb598d83212038ecbe43adba45f0a7b3768433f
Ainis Taranda <taranda.ainis@gmail.com>
2022-10-29 09:20:57 -0400

    finished

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,11 @@
-import functions  from "firebase-functions"
+import functions from "firebase-functions"
 import express from 'express'
+import cors from 'cors'
 import { createClothing, updateClothing, getAllClothing, deleteClothing, findOneItem } from './src/clothing.js'
 
 const app = express()
 app.use(express.json())
-
+app.use(cors())
 app.post('/clothing', createClothing)
 app.get('/clothing', getAllClothing)
 app.patch('/clothing/:uid', updateClothing)
